ca7d042badb4f4310cba0339b571f2ffb408b7eb
Zeljko <zeljko@zwr.fi>
2014-11-17 20:11:04 +0200

    search by categories and by models
    When manufacturer is specified, we will select only
    those parts from the text selection that belong to
    any car of that manufacturer. After that, similarly we
    select only those previously selected products that
    belongs to any selected category or any of its
    subcategories.
    
    Signed-off-by: Zeljko <zeljko@zwr.fi>

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -24,8 +24,28 @@ class SearchController < ApplicationController
     @response[:products_url] = "/products/"
     
     criteria = s.split.select{|word| word.length >= 4}.map{|word| "name like '%#{word}%'"}.join(" or")
+    models_to_search = []
+    categories_to_search = []
+    params[:selected_groups].each do |g|
+      case g[:type]
+      when "manufacturer"
+        models_to_search.push(*(Manufacturer.find(g[:id]).car_brands))
+      when "category"
+        c = Category.find(g[:id])
+        include_this_and_children categories_to_search, c
+      end
+    end unless params[:selected_groups].nil?
+    
+    criteria = "2>1" if criteria.empty?
+    criteria = "#{criteria} and id in (select product_id from car_brands_products where car_brand_id in (#{models_to_search.empty? ? 0 : models_to_search.map{|m|m.id}.join(",")}))" unless models_to_search.empty?
+    criteria = "#{criteria} and id in (select product_id from categories_products where category_id in (#{categories_to_search.empty? ? 0 : categories_to_search.map{|m|m.id}.join(",")}))" unless categories_to_search.empty?
     puts "Product search: #{criteria}"
-    @response[:products] = Product.where(criteria).map { |p| 
+    
+    
+    
+    selected_products = Product.where(criteria)
+
+    @response[:products] = selected_products.map { |p| 
       {
         name: p.name,
         id: p.id,
@@ -38,5 +58,10 @@ class SearchController < ApplicationController
     respond_to do |format|
       format.json { render json: @response }
     end
+  end
+  
+  def include_this_and_children(categories_to_search, c)
+    categories_to_search.push(*c)
+    c.children.each { |child| include_this_and_children(categories_to_search, child) }
   end
 end
