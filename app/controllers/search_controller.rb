174bc244ad9d9f77fbeb20bc7a2f74b06bfd4388
Zeljko <zeljko@zwr.fi>
2014-11-17 17:00:22 +0200

    search criteria processing, first hit
    Signed-off-by: Zeljko <zeljko@zwr.fi>

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,8 +5,18 @@ class SearchController < ApplicationController
   end
 
   def query
-    puts params
+    s = params[:text]
+    puts "Text criteria: #{s}"
     @response = { under_development: true }
+    @response[:groups] = []
+    @response[:groups] << {
+      name: "Select car model",
+      sub: Manufacturer.all.map { |m| { name: m.name, type: :manufacturer } }
+    }
+    @response[:groups] << {
+      name: "Categories",
+      sub: Category.where(parent_id: 1).map { |m| { name: m.name, type: :category } }
+    }
     respond_to do |format|
       format.json { render json: @response }
     end
